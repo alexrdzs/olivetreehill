59b8dbc93c594a6218267f60f5bffb668f04d3e3
alex rodríguez santibáñez <alex@alexrds.com>
2017-12-20 12:00:27 +0100

    Versión 20 12 17 - Dev Olive Tree Hill

--- functions.php
+++ functions.php
@@ -160,3 +160,24 @@ function hide_update_notice() {
     }
 }
 add_action( 'admin_notices', 'hide_update_notice', 1 );
+remove_filter( 'map_meta_cap', 'flamingo_map_meta_cap' );
+add_filter( 'map_meta_cap', 'mycustom_flamingo_map_meta_cap', 9, 4 );
+
+function mycustom_flamingo_map_meta_cap( $caps, $cap, $user_id, $args ) {
+    $meta_caps = array(
+        'flamingo_edit_contacts' => 'publish_posts',
+        'flamingo_edit_contact' => 'publish_posts',
+        'flamingo_delete_contact' => 'publish_posts',
+        'flamingo_edit_inbound_messages' => 'publish_posts',
+        'flamingo_delete_inbound_message' => 'publish_posts',
+        'flamingo_delete_inbound_messages' => 'publish_posts',
+        'flamingo_spam_inbound_message' => 'publish_posts',
+        'flamingo_unspam_inbound_message' => 'publish_posts' );
+
+    $caps = array_diff( $caps, array_keys( $meta_caps ) );
+
+    if ( isset( $meta_caps[$cap] ) )
+        $caps[] = $meta_caps[$cap];
+
+    return $caps;
+}
